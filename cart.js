50fc9e87c1d2448a268a0311a83e8c88cbe37078
Andrew Elliott <andrewelliott159@gmail.com>
2022-01-28 10:22:06 -0700

    Initial commit

---
+++ cart.js
@@ -0,0 +1,109 @@
+///////////////////////////////////////////////
+///////////////////CART.JS/////////////////////
+///////////////////////////////////////////////
+/*
+    In this file, you'll be writing code to
+    calculate order totals. You'll also be 
+    creating customer objects.  
+*/
+
+
+//////////////////PROBLEM 1////////////////////
+/*  
+    Below is a cart array that has food objects
+    inside. 
+
+    Write a callback below that uses the reduce
+    array method to calculate the sum of all
+    the food. 
+*/
+
+const cart = [
+    {
+        name: 'pizza', 
+        price: 9.99
+    }, 
+    {
+        name: 'pasta', 
+        price: 8.99
+    }, 
+    {
+        name: 'salad', 
+        price: 7.99
+    }
+]
+
+//CODE HERE
+const summedPrice = cart.reduce((acc, curr) => {
+    return acc + curr.price;
+}, 0);
+
+
+//////////////////PROBLEM 2////////////////////
+/*  
+    Write a function called `calcFinalPrice` that
+    can take in `cartTotal`,`couponValue`,
+    and `tax` arguments. 
+
+    Inside the function, calculate the tax 
+    on the cartTotal and add it in. Subtract
+    the value of the coupon. Return the final
+    number. 
+
+    Note: the numbers passed in for `tax` will be
+    decimals, for example: .06 for a 6% tax.
+*/
+
+//CODE HERE
+const calcFinalPrice = (cartTotal, couponValue, tax) => {
+    newTotal = cartTotal * (1 + tax);
+    newTotal -= couponValue;
+
+    return newTotal;
+}
+
+
+//////////////////PROBLEM 3////////////////////
+/*  
+    In this problem, you'll create a model for 
+    a customer object as well as an example
+    object. 
+
+    Plan out a customer object for the cart page.
+    Think about the information that a 
+    restaurant would need about its customers.
+
+    In the TEXT ANSWER area below, describe the
+    properties that your customer object will have
+    and why you chose those properties.
+
+    Explain what data types each property should be
+    and why you chose those data types. 
+
+    Your object should have at least 4 properties. 
+*/
+
+/*
+    TEXT ANSWER HERE
+
+    id: a way for the code to indentify each user.
+    name: a display name for the customer, but not the identifier, mosstly for the customer to see on a profile page.
+    location: a zip code to locate the customer.
+    rewardsPoints: points the customer has accumulated in the rewards program.
+    customerTier: the customer's level in the rewards program.
+
+*/
+
+/*
+    Now, create a customer object following your own
+    guidelines.
+*/
+
+//CODE HERE
+const customer = {
+    id: 8808,
+    name: "Andrew",
+    location: 29579,
+    rewardsPoints: 544,
+    customerTier: "Platnium"
+}
